9254e3a070f91bb2128c8710437ace48cc2276c7
Petr153 <lipus.petr@seznam.cz>
2026-03-16 10:39:30 +0100

    Delete demo directory

--- demo/src/main/java/cz/osu/demo/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.osu.demo.repositories;
-
-import cz.osu.demo.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
